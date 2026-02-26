c92ae78099d707e8fc08b5e6e8878fc4fb5666fd
chitranshsingh01 <chitranshsingh02@gmail.com>
2026-02-26 16:18:17 +0000

    Sync LeetCode submission Runtime - 4 ms (82.38%), Memory - 46.4 MB (34.03%)

---
+++ 0242-valid-anagram/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+     public static boolean isAnagram(String s, String t) {
+        char[] a=s.toCharArray();
+        char[] b=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    } 
+}
