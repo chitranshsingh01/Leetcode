1946b5d7116118b9206621456dd85ecc71205307
chitranshsingh01 <chitranshsingh02@gmail.com>
2026-02-27 18:37:32 +0000

    Sync LeetCode submission Runtime - 5 ms (61.67%), Memory - 44.5 MB (67.08%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,9 +1,19 @@
 class Solution {
-     public static boolean isAnagram(String s, String t) {
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
-    } 
-}
+    public static boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+
+                int[] count = new int[26];   // for lowercase letters
+
+                    for (int i = 0; i < s.length(); i++) {
+                            count[s.charAt(i) - 'a']++;
+                                    count[t.charAt(i) - 'a']--;
+                                        }
+
+                                            for (int c : count) {
+                                                    if (c != 0) return false;
+                                                        }
+
+                                                            return true;
+                                                            }
+    }
+
