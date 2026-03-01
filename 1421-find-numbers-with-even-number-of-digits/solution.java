a08373ed71b9530644f0eb130bc9642c2e59eda2
chitranshsingh01 <chitranshsingh02@gmail.com>
2026-03-01 15:27:41 +0000

    Sync LeetCode submission Runtime - 1 ms (99.53%), Memory - 44.4 MB (89.86%)

--- 1421-find-numbers-with-even-number-of-digits/solution.java
+++ 1421-find-numbers-with-even-number-of-digits/solution.java
@@ -1,19 +1,5 @@
 import java.util.Scanner;
 class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         System.out.println("enter the number of element ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.println("enter the array");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=sc.nextInt();
-        }
-
-        int ans= findNumbers(arr);
-        System.out.println("the num of element with even digits are:" + ans);
-
-    }
     public static int findNumbers(int[] nums){
           int count=0;
         for (int i = 0; i < nums.length; i++) {
@@ -27,7 +13,7 @@ class Solution {
         int b=0;
         while (a>0){
             a=a/10;
-            b=b+1;
+            b++;
         }
         if (b%2==0) {
             return true;
