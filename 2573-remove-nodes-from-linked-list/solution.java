fa887683440c562dd66de6832234f0177387d350
chitranshsingh01 <chitranshsingh02@gmail.com>
2026-04-10 17:34:39 +0000

    Sync LeetCode submission Runtime - 77 ms (11.01%), Memory - 134.6 MB (30.35%)

--- 2573-remove-nodes-from-linked-list/solution.java
+++ 2573-remove-nodes-from-linked-list/solution.java
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public static ListNode removeNodes(ListNode head) {
          //  [5,2,13,3,8]
