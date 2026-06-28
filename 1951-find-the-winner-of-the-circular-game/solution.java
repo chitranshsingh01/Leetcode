225223d7622cc7acc40661ac700ca0520d6e00a5
chitranshsingh01 <chitranshsingh02@gmail.com>
2026-06-28 18:36:03 +0000

    Sync LeetCode submission Runtime - 44 ms (24.43%), Memory - 46.6 MB (7.00%)

--- 1951-find-the-winner-of-the-circular-game/solution.java
+++ 1951-find-the-winner-of-the-circular-game/solution.java
@@ -2,16 +2,16 @@ import java.util.Queue;
 import java.util.LinkedList;
 class Solution {
     public int findTheWinner(int n, int k) {
- Queue<Integer> q = new LinkedList<>();
-       for(int i=1;i<=n;i++){
-        q.add(i);
-      }
-      while(q.size()!=1){
-        for(int i=1;i<k;i++){
-            q.add(q.remove());
-        }
-        q.remove();
-      }
-      return q.peek();
-    }
-}
+     Queue<Integer> q = new LinkedList<>();
+            for(int i=1;i<=n;i++){
+                    q.add(i);
+                          }
+                                while(q.size()!=1){
+                                        for(int i=1;i<k;i++){
+                                                    q.add(q.remove());
+                                                            }
+                                                                    q.remove();
+                                                                          }
+                                                                                return q.peek();
+                                                                                    }
+                                                                                    }
